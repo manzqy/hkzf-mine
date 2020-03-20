c0046056861567cd28a685283086a20cb8085a2f
xiaoYu <18839199638@163.com>
2020-03-20 09:14:54 +0800

    初始化资源

---
+++ src/pages/found/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Found extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return ( 
+      <div>Found</div>
+     );
+  }
+}
+ 
+export default Found;
